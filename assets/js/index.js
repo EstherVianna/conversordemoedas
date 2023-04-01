88c05fc00c810077d3fea8430805624a45a99b64
EstherVianna <esther.avianna@gmail.com>
2023-03-31 21:55:13 -0300

    fixed some bugs

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,19 +2,19 @@ function currencyPage(){
     const options = document.querySelector(".options");
 
     if(options.value == "btc"){
-        window.location.assign("/moedas/Bitcoin/bitcoin.html");
+        window.location.assign("moedas/Bitcoin/bitcoin.html");
     }
     else if(options.value == "dolar"){
-        window.location.assign("/moedas/Dolar/dolar.html");
+        window.location.assign("moedas/Dolar/dolar.html");
     }
     else if(options.value == "euro"){
-        window.location.assign("/moedas/Euro/euro.html");
+        window.location.assign("moedas/Euro/euro.html");
     }
     else if(options.value == "libra"){
-        window.location.assign("/moedas/Libra/libra.html");
+        window.location.assign("moedas/Libra/libra.html");
     }
     else if(options.value == "peso"){
-        window.location.assign("/moedas/Peso/peso.html");
+        window.location.assign("moedas/Peso/peso.html");
     }
     else{
         alert("Selecione uma moeda")
