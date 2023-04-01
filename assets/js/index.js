f6e30e7ca934ab7fb6e02b69a34b24b8c1730cdb
EstherVianna <esther.avianna@gmail.com>
2023-03-31 21:34:15 -0300

    first-commit

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+function currencyPage(){
+    const options = document.querySelector(".options");
+
+    if(options.value == "btc"){
+        window.location.assign("/moedas/Bitcoin/bitcoin.html");
+    }
+    else if(options.value == "dolar"){
+        window.location.assign("/moedas/Dolar/dolar.html");
+    }
+    else if(options.value == "euro"){
+        window.location.assign("/moedas/Euro/euro.html");
+    }
+    else if(options.value == "libra"){
+        window.location.assign("/moedas/Libra/libra.html");
+    }
+    else if(options.value == "peso"){
+        window.location.assign("/moedas/Peso/peso.html");
+    }
+    else{
+        alert("Selecione uma moeda")
+    }
+
+}
---
+++ moedas/Dolar/dolar.js
@@ -0,0 +1,68 @@
+function Convert() {
+    const coins = [
+      "EUR-BRL",
+      "USD-BRL",
+      "BTC-BRL",
+      "ARS-BRL",
+      "GBP-BRL"
+    ];
+    const url = "https://economia.awesomeapi.com.br/json/last/";
+    const formatCurrency = (value) => {
+        return value.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+      }
+    
+    const result = document.querySelector("#resultado")
+    const inputValue = document.querySelector("#valor").value
+    const date = document.querySelector("#date")
+    
+      if (result != null){
+        result.remove()
+        inputValue == ""
+        date.remove()
+      }
+  
+    fetch(url + coins)
+      .then(function(response) {
+        if (!response.ok) {
+          throw new Error("Erro ao obter dados da API: " + response.statusText);
+        }
+        return response.json();
+      })
+      .then(function(data) {
+        const value = parseFloat(document.querySelector("#valor").value);
+
+        if(value > 0 ){
+        let dolarReal = data.USDBRL;
+        const currency = dolarReal.ask;
+        const elementoPai = document.querySelector(".container");
+        const elementoFilho = document.createElement("input");
+        const resposta = `${value} dólares é igual a ${formatCurrency(value * currency)} reais`;
+
+        elementoFilho.id = "resultado";
+  
+        elementoFilho.setAttribute("disabled", null);
+  
+        elementoPai.appendChild(elementoFilho);
+  
+        elementoFilho.value = resposta;
+        
+        const paragraph = document.createElement("p");
+        paragraph.id = "date"
+        const date =  new Date (dolarReal.create_date);
+
+        const LastUpdate = paragraph.innerHTML =
+                         `Última atualização ${date.toLocaleString()}`;
+
+        elementoPai.appendChild(paragraph);
+
+        return resposta, LastUpdate;
+      
+      }
+      else{
+        alert(`Insira um valor válido`)}
+    })
+      .catch(function(error) {
+        console.error(error.message);
+      });
+
+}
